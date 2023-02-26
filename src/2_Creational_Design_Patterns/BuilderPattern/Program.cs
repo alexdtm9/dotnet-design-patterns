243f17a5af8fb7ad2458cc696bb87640616a2257
alexdtm9 <a.m.talpau@gmail.com>
2023-02-26 10:53:03 +0100

    :construction: Builder Pattern

---
+++ src/2_Creational_Design_Patterns/BuilderPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using BuilderPattern;
+
+// Without builder
+var hello = "Hello";
+var sb = new StringBuilder();
+sb.Append("<p>");
+sb.Append(hello);
+sb.Append("</p>");
+Console.WriteLine(sb);
+
+var words = new[] { "hello", "world" };
+sb.Clear();
+sb.Append("<ul>");
+foreach (var word in words)
+{
+    sb.Append($"<li>{word}</li>");
+}
+
+sb.Append("</ul>");
+Console.WriteLine(sb);
+
+// With builder
+var builder = new HtmlBuilder("ul");
+builder
+    .AddChild("li", "hello")
+    .AddChild("li", "world"); // Fluent interface
+Console.WriteLine(builder.ToString());    
